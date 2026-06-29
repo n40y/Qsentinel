10d790cd73995046625790c4a25f677d30c1c8fb
N40y <131626661+n40y@users.noreply.github.com>
2026-06-29 20:37:48 +0200

    Update html_report.py

--- report/html_report.py
+++ report/html_report.py
@@ -1,22 +1,20 @@
 # report/html_report.py
 
-from jinja2 import Environement, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader
 from os import path
 
-# Génère un rapport html à partir des données obtenues
+
 def generate_report(results: dict, output: str) -> None:
-    
+
     env = Environment(loader=FileSystemLoader(path.dirname(__file__)))
     template = env.get_template("report_template.html")
-    
-     # Prépare les données pour le template
+
     tls = results.get("tls", {})
     classical = results.get("classical", {})
     quantum = results.get("quantum", {})
     recommendations = results.get("recommendations", {})
     score = results.get("score", 0)
 
-    # Rend le template
     html = template.render(
         host=tls.get("host", "Inconnu"),
         port=tls.get("port", 0),
@@ -31,6 +29,5 @@ def generate_report(results: dict, output: str) -> None:
         score=score,
     )
 
-    # Écrit le rapport
     with open(output, "w") as f:
         f.write(html)
